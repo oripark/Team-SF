592ceffb7e68b0fa09f85916abbd022c60e3d2e0
kingdavy1 <58772409+kingdavy1@users.noreply.github.com>
2019-12-11 21:23:48 +0900

    Delete create_dict.cs

--- create_dict.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class create_dict : MonoBehaviour
-{
-    public static Dictionary<string, bool> animal_list = new Dictionary<string, bool>();
-    public static Dictionary<string, bool> hangul_list = new Dictionary<string, bool>();
-}
